b0919aefae4c40cd1c59a9fd4fb3e4d14e5e7327
Eric London <git@ericlondon.com>
2013-11-19 10:26:36 -0500

    revised example script

--- example.rb
+++ example.rb
@@ -7,6 +7,8 @@ $:.unshift File.dirname(__FILE__)
 
 require 'exec_with_timeout.rb'
 
-command = %{ruby -e "(1..100).each {|x| puts x; sleep 1}"}
-output = Exec_With_Timeout.exec command, 10
+command = "./test.rb"
+output = Exec_With_Timeout.exec command, 5
 puts output
+
+#loop {}
